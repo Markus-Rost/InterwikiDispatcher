061441ac1065d307af9b18065ab5928411ebe17b
Markus-Rost <37275477+Markus-Rost@users.noreply.github.com>
2023-06-23 23:10:34 +0200

    finished? maybe?

---
+++ README.md
@@ -0,0 +1,58 @@
+# InterwikiDispatcher extension
+
+This is free software licensed under the GNU General Public License. Please
+see http://www.gnu.org/copyleft/gpl.html for further details, including the
+full text and terms of the license.
+
+## Overview
+Adding some simple multi-level interwikis for easier linking to wiki farms.
+
+Hooking into existing interwiki prefixes to provide some limited API support. Subdomain part of the interwiki is validated as `a-z\d-` to avoid open redirect vulnerabilities.
+
+## Example configs
+```php
+# [[w:c:minecraft]] => https://minecraft.fandom.com/wiki/
+# [[w:c:minecraft:Cookie]] => https://minecraft.fandom.com/wiki/Cookie
+# [[w:c:de.minecraft:Keks]] => https://minecraft.fandom.com/de/wiki/Keks
+$wgIWDPrefixes[] = [
+  'interwiki' => 'w', # Interwiki prefix that exists in the interwiki table
+  'subprefix' => 'c', # Optional: Subprefix to keep the interwiki working mostly as expected
+  'url' => 'https://$2.fandom.com/wiki/$1', # URL format for the interwiki
+  'urlInt' => 'https://$2.fandom.com/$3/wiki/$1' # Optional: Additional URL format for the interwiki
+];
+```
+```php
+# [[mh:meta]] => https://meta.miraheze.org/wiki/
+# [[mh:meta:Miraheze]] => https://meta.miraheze.org/wiki/Miraheze
+$wgIWDPrefixes[] = [
+  'interwiki' => 'mh',
+  'url' => 'https://$2.miraheze.org/wiki/$1'
+];
+```
+```php
+# [[farm:mh:meta]] => https://meta.miraheze.org/wiki/
+# [[farm:mh:meta:Miraheze]] => https://meta.miraheze.org/wiki/Miraheze
+$wgIWDPrefixes[] = [
+  'interwiki' => 'farm',
+  'subprefix' => 'mh',
+  'url' => 'https://$2.miraheze.org/wiki/$1'
+];
+# [[farm:fd:minecraft]] => https://minecraft.fandom.com/wiki/
+# [[farm:fd:minecraft:Cookie]] => https://minecraft.fandom.com/wiki/Cookie
+# [[farm:fd:de.minecraft:Keks]] => https://minecraft.fandom.com/de/wiki/Keks
+$wgIWDPrefixes[] = [
+  'interwiki' => 'farm',
+  'subprefix' => 'fd',
+  'url' => 'https://$2.fandom.com/wiki/$1',
+  'urlInt' => 'https://$2.fandom.com/$3/wiki/$1'
+];
+# [[farm:gg:terraria]] => https://terraria.fandom.com/wiki/
+# [[farm:gg:terraria:NPCs]] => https://terraria.fandom.com/wiki/NPCs
+# [[farm:gg:de.terraria:NPCs]] => https://terraria.fandom.com/de/wiki/NPCs
+$wgIWDPrefixes[] = [
+  'interwiki' => 'farm',
+  'subprefix' => 'gg',
+  'url' => 'https://$2.wiki.gg/wiki/$1',
+  'urlInt' => 'https://$2.wiki.gg/$3/wiki/$1'
+];
+```
--- includes/InterwikiDispatcherHooks.php
+++ includes/InterwikiDispatcherHooks.php
@@ -4,6 +4,12 @@ namespace MediaWiki\Extension\InterwikiDispatcher;
 use Title;
 
 class InterwikiDispatcherHooks implements \MediaWiki\Hook\GetLocalURLHook {
+    private array $rules;
+
+    public function __construct( Config $config ) {
+        $this->rules = $config->get( 'IWDPrefixes' );
+    }
+
     /**
      * @param Title $title Title object of page
      * @param string &$url String value as output (out parameter, can modify)
@@ -11,7 +17,16 @@ class InterwikiDispatcherHooks implements \MediaWiki\Hook\GetLocalURLHook {
      * @return bool|void True or no return value to continue or false to abort
      */
     public function onGetLocalURL( $title, &$url, $query ) {
-        if ( $title->getInterwiki() !== 'gg' ) {
+        foreach ( $this->rules as $key => $rule ) {
+            if ( $this->onGetLocalURLSingle( $title, $url, $query, $rule ) === false ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function onGetLocalURLSingle( $title, &$url, $query, $rule ) {
+        if ( $title->getInterwiki() !== $rule['interwiki'] ) {
             return true;
         }
         $namespace = $title->getNsText();
@@ -21,18 +36,25 @@ class InterwikiDispatcherHooks implements \MediaWiki\Hook\GetLocalURLHook {
             $namespace .= ':';
         }
         $dbkey = $namespace . $title->getDBKey();
+        $subprefix = $rule['subprefix'] ?? '';
+        if ( $subprefix !== '' ) $subprefix .= '_*:_*';
         $m = [];
-
-        if ( !empty( $dbkey ) && preg_match( "/^c_*:_*(?:([a-z-]{2,12})\.)?([a-z\d-]{1,50})(?:_*:_*(.*))?$/Si", $dbkey, $m ) ) {
+        if ( !empty( $dbkey ) && preg_match( "/^$subprefix(?:([a-z-]{2,12})\.)?([a-z\d-]{1,50})(?:_*:_*(.*))?$/Si", $dbkey, $m ) ) {
             if ( !isset( $m[3] ) ) $m[3] = '';
             [ , $language, $wiki, $article ] = $m;
-            $wiki = strtolower( $wiki );
             if ( empty( $language ) ) {
-                $articlePath = "https://$wiki.wiki.gg/wiki/$1";
-            } else {
-                $language = strtolower( $language );
-                $articlePath = "https://$wiki.wiki.gg/$language/wiki/$1";
+                # $articlePath = 'https://$2.wiki.gg/wiki/$1'
+                $articlePath = $rule['url'];
             }
+            else {
+                # $articlePath = 'https://$2.wiki.gg/$3/wiki/$1'
+                $articlePath = $rule['urlInt'];
+                if ( !isset( $articlePath ) ) {
+                    return true;
+                }
+                $articlePath = str_replace( "$3", strtolower( $language ), $articlePath );
+            }
+            $articlePath = str_replace( "$2", strtolower( $wiki ), $articlePath );
             $url = str_replace( "$1", wfUrlencode( $article ?? '' ), $articlePath );
             $url = wfAppendQuery( $url, $query );
             return false;
