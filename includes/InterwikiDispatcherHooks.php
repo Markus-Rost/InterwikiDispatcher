04f18fe2ff06fcfc732c223e90214fdefc2f5d77
Markus-Rost <37275477+Markus-Rost@users.noreply.github.com>
2023-06-23 21:35:13 +0200

    require subprefix

--- includes/InterwikiDispatcherHooks.php
+++ includes/InterwikiDispatcherHooks.php
@@ -11,8 +11,8 @@ class InterwikiDispatcher implements \MediaWiki\Hook\GetLocalURLHook {
      * @return bool|void True or no return value to continue or false to abort
      */
     public function onGetLocalURL( $title, &$url, $query ) {
-        if ( $title->getInterwiki() != 'gg' ) {
-            return;
+        if ( $title->getInterwiki() !== 'gg' ) {
+            return true;
         }
         $namespace = $title->getNsText();
         if ( $namespace != '' ) {
@@ -21,31 +21,22 @@ class InterwikiDispatcher implements \MediaWiki\Hook\GetLocalURLHook {
             $namespace .= ':';
         }
         $dbkey = $namespace . $title->getDBKey();
-        if ( $dbkey == '' ) {
-            $url = 'https://support.wiki.gg/wiki/';
-            return;
-        }
         $m = [];
-        if ( preg_match( "/^((?:[a-z-]{2,12}\.)?[a-z\d-]{1,50})(?:_*:_*(.*))?$/Si", $dbkey, $m ) ) {
-            $wiki = explode( '.', strtolower( $m[1] ) );
-            if ( count( $wiki ) > 2 ) {
-                return false;
+
+        if ( !empty( $dbkey ) && preg_match( "/^c_*:_*(?:([a-z-]{2,12})\.)?([a-z\d-]{1,50})(?:_*:_*(.*))?$/Si", $dbkey, $m ) ) {
+            if ( !isset( $m[3] ) ) $m[3] = '';
+            [ , $language, $wiki, $article ] = $m;
+            $wiki = strtolower( $wiki );
+            if ( empty( $language ) ) {
+                $articlePath = "https://$wiki.wiki.gg/wiki/$1";
+            } else {
+                $language = strtolower( $language );
+                $articlePath = "https://$wiki.wiki.gg/$language/wiki/$1";
             }
-            if ( count( $wiki ) == 2 ) {
-                $wiki = "https://$wiki[1].wiki.gg/$wiki[0]/$1";
-            }
-            else {
-                $wiki = "https://$wiki[0].wiki.gg/$1";
-            }
-            if ( !isset( $m[2] ) ) {
-                $m[2] = '';
-            }
-            $url = str_replace( "$1", wfUrlencode( $m[2] ), $wiki );
+            $url = str_replace( "$1", wfUrlencode( $article ?? '' ), $articlePath );
             $url = wfAppendQuery( $url, $query );
-        }
-        else {
             return false;
         }
-        return;
+        return true;
     }
 }
